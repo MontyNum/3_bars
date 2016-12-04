2e5297d5824ac74246c54d28f017ed0a1e966e3f
MontyNum <numpymonty@yandex.ru>
2016-12-05 00:37:39 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -2,19 +2,24 @@ import json
 
 
 def load_data(filepath):
-    pass
+    return json.load(open(filepath, 'r'))
 
 
 def get_biggest_bar(data):
-    pass
+    bars = [(bar['SeatsCount'], bar['Name']) for bar in data]
+    return [name for cnt, name in bars if cnt == max(bars)[0]]
 
 
 def get_smallest_bar(data):
-    pass
+    bars = [(bar['SeatsCount'], bar['Name']) for bar in data]
+    return [name for cnt, name in bars if cnt == min(bars)[0]]
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    '''Евклидовы расстояния'''
+    bars = [((bar['geoData']['coordinates'][0]-longitude)**2 + (bar['geoData']['coordinates'][1]-latitude)**2, bar['Name']) \
+            for bar in data]
+    return min(bars)[1]
 
 
 if __name__ == '__main__':
