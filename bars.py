1c0f92426f7d7cc4133253a22ecb01d5b4d92162
MontyNum <numpymonty@yandex.ru>
2016-12-08 00:26:04 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 
-def load_data(filepath):
+def load_data_of_bars(filepath):
     if not os.path.exists(filepath):
         print('There is no such path or file. Exiting...')
         sys.exit()
@@ -13,12 +13,14 @@ def load_data(filepath):
 
 def get_biggest_bar(json_content):
     bars = [(bar['SeatsCount'], bar['Name']) for bar in json_content]
-    return [name for seats_count, name in bars if seats_count == max(bars)[0]]
+    biggest_bars = [bar_name for bar_seats_count, bar_name in bars if bar_seats_count == max(bars)[0]]
+    return biggest_bars
 
 
 def get_smallest_bar(json_content):
     bars = [(bar['SeatsCount'], bar['Name']) for bar in json_content]
-    return [name for seats_count, name in bars if seats_count == min(bars)[0]]
+    smallest_bars = [bar_name for bar_seats_count, bar_name in bars if bar_seats_count == min(bars)[0]]
+    return smallest_bars
 
 
 def get_closest_bar(json_content, longitude, latitude):
@@ -29,18 +31,19 @@ def get_closest_bar(json_content, longitude, latitude):
     bars = [([abs(bar['geoData']['coordinates'][0]-longitude), abs(bar['geoData']['coordinates'][1]-latitude)], bar['Name']) \
             for bar in json_content]
     distances = []
-    for angles, name in bars:
-        if angles[1]>180.:                                    # carefully
-            angles[1] = 360. - angles[1]
-        distances.append((angles[0]**2 + angles[1]**2, name)) # square of angle distance
-    return [name for distance, name in distances if distance == min(distances)[0]]
+    for bar_coordinates, bar_name in bars:
+        if bar_coordinates[1] > 180.:                                    # carefully
+            bar_coordinates[1] = 360. - bar_coordinates[1]
+        distances.append((bar_coordinates[0]**2 + bar_coordinates[1]**2, bar_name)) # square of angle distance
+    closest_bars = [bar_name for bar_distance, bar_name in distances if bar_distance == min(distances)[0]]
+    return closest_bars
 
 
 if __name__ == '__main__':
     file_path = input('Enter path of file: ')
-    data = load_data(file_path)
-    print('\nThe biggest bar:', get_biggest_bar(data))
-    print('The smallest bar:', get_smallest_bar(data), '\n')
+    bars = load_data_of_bars(file_path)
+    print('\nThe biggest bar:', get_biggest_bar(bars))
+    print('The smallest bar:', get_smallest_bar(bars), '\n')
     longitude = float(input('Enter longitude of interesting position: '))
     latitude = float(input('Enter latitude of interesting position: '))
-    print('\nThe closest bar:', get_closest_bar(data, longitude, latitude))
+    print('\nThe closest bar:', get_closest_bar(bars, longitude, latitude))
