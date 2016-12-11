af303ab5582e9a44b1579bd7196da156b2b49d1e
MontyNum <numpymonty@yandex.ru>
2016-12-12 01:26:16 +0300

    Update bars.py

--- bars.py
+++ bars.py
@@ -3,39 +3,51 @@ import sys
 import os
 
 
-def load_data_of_bars(filepath):
-    if not os.path.exists(filepath):
+def load_data_of_bars(json_filepath):
+    if not os.path.exists(json_filepath):
         print('There is no such path or file. Exiting...')
         sys.exit()
-    with open(filepath, 'r') as file_handler:
-        return json.load(file_handler)
+    with open(json_filepath, 'r') as file_handler:
+        bars_content = json.load(file_handler)
+        return bars_content
 
+def get_bar_seat_counts(bars_content):
+    bar_seat_counts = [(bar['SeatsCount'], bar['Name']) for bar in bars_content]
+    return bar_seat_counts
 
-def get_biggest_bar(json_content):
-    bars = [(bar['SeatsCount'], bar['Name']) for bar in json_content]
-    biggest_bars = [bar_name for bar_seats_count, bar_name in bars if bar_seats_count == max(bars)[0]]
+def get_biggest_bar(bar_seat_counts):
+    biggest_bars = [bar_name for bar_seat_count, bar_name in bar_seat_counts if bar_seat_count == max(bar_seat_counts)[0]]
     return biggest_bars
 
 
-def get_smallest_bar(json_content):
-    bars = [(bar['SeatsCount'], bar['Name']) for bar in json_content]
-    smallest_bars = [bar_name for bar_seats_count, bar_name in bars if bar_seats_count == min(bars)[0]]
+def get_smallest_bar(bar_seat_counts):
+    smallest_bars = [bar_name for bar_seat_count, bar_name in bar_seat_counts if bar_seat_count == min(bar_seat_counts)[0]]
     return smallest_bars
 
 
-def get_closest_bar(json_content, longitude, latitude):
+def get_bars_coordinates(bars_content):
+    bars_coordinates = [([abs(bar['geoData']['coordinates'][0] - longitude), abs(bar['geoData']['coordinates'][1] - latitude)], \
+                         bar['Name']) for bar in bars_content]
+    return bars_coordinates
+
+
+def get_angle_distances(bars_coordinates, longitude, latitude):
+    distances = []
+    for bar_coordinates, bar_name in bars_coordinates:
+        if bar_coordinates[1] > 180.:                                    # carefully
+            bar_coordinates[1] = 360. - bar_coordinates[1]
+        distances.append((bar_coordinates[0]**2 + bar_coordinates[1]**2, bar_name)) # square of angle distance
+    return distances
+        
+        
+def get_closest_bar(bars_content, longitude, latitude):
     '''
     -90 <= longitude <= 90
     -180 <= latitude <= 180
     '''
-    bars = [([abs(bar['geoData']['coordinates'][0]-longitude), abs(bar['geoData']['coordinates'][1]-latitude)], bar['Name']) \
-            for bar in json_content]
-    distances = []
-    for bar_coordinates, bar_name in bars:
-        if bar_coordinates[1] > 180.:                                    # carefully
-            bar_coordinates[1] = 360. - bar_coordinates[1]
-        distances.append((bar_coordinates[0]**2 + bar_coordinates[1]**2, bar_name)) # square of angle distance
-    closest_bars = [bar_name for bar_distance, bar_name in distances if bar_distance == min(distances)[0]]
+    bars_coordinates = get_bars_coordinates(bars_content)
+    angle_distances = get_angle_distances(bars_coordinates, longitude, latitude)
+    closest_bars = [bar_name for bar_distance, bar_name in angle_distances if bar_distance == min(angle_distances)[0]]
     return closest_bars
 
 
